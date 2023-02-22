a07c796010485edcf4986b9f83f92991faaef191
pavadomino <pavadomino@gmail.com>
2023-02-22 19:42:42 +0100

    Add Gemfile, Rakefile and add Product model

--- app.rb
+++ app.rb
@@ -1,7 +1,12 @@
-#encoding: utf-8
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, {adapter: "sqlite3", database: "pizzashop.db"}
+
+class Product < AcriveRecord::Base
+end
 
 get '/' do
   erb :index
