707f7f904ed53195479b9b82433468de7444982a
pavadomino <pavadomino@gmail.com>
2023-02-22 19:29:27 +0100

    Keep variable x in local storage

--- public/app.js
+++ public/app.js
@@ -1,6 +1,7 @@
 function something()
 {
   var x = window.localStorage.getItem('aaa');
-  window.localStorage.setItem('aaa', 555);
+  x = x * 1 +1
+  window.localStorage.setItem('aaa', x);
   alert(x);
 }
