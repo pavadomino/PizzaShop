22843fba40f48152b46fc221a1691f9c33113b79
pavadomino <pavadomino@gmail.com>
2023-02-26 21:47:00 +0100

    Update cart button

--- public/app.js
+++ public/app.js
@@ -13,6 +13,13 @@ function add_to_cart(id)
   x = x * 1 + 1;
   window.localStorage.setItem(key, x);
   update_orders_input();
+  update_orders_button();
+}
+
+function update_orders_button()
+{
+  var text = 'Cart(' + cart_get_number_of_items() + ')';
+  $('#orders_button').val(text);
 }
 
 function update_orders_input()
