63cdfb90bfa4ccc47c90843f727b39cd710159df
pavadomino <pavadomino@gmail.com>
2023-02-26 21:17:50 +0100

    Add function cart_get_orders

--- public/app.js
+++ public/app.js
@@ -28,3 +28,17 @@ function cart_get_number_of_items()
   }
   return total;
 }
+
+function cart_get_orders()
+{
+  var orders = '';
+  for (var i = 0; i < window.localStorage.length; i++){
+    var key = window.localStorage.key(i);
+    var value = window.localStorage.getItem(key);
+    if (key.indexOf('product_') == 0)
+    {
+      orders = orders + key + '=' + value + ',';
+    }
+  }
+  return orders;
+}
